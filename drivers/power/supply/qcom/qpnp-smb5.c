ab606ebb153c857a34f4f04d13dd6791c57a8b74
LuK1337 <priv.luk@gmail.com>
2025-08-03 13:58:45 +0200

    power: smb5: Add battchg_ext class for smart_charging_interruption
    Change-Id: I3975603903817789c809b7dbb4bad449001b959c

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -3923,6 +3923,38 @@ static void smb5_somc_remove_sysfs_entries(struct device *dev)
 		device_remove_file(dev, &smb5_somc_attrs[i]);
 }
 
+static ssize_t smart_charging_interruption_store(struct class *class,
+				struct class_attribute *attr, const char *buf, size_t count)
+{
+	struct smb_charger *chg = container_of(class, struct smb_charger, bcext_class);
+	int val = 0;
+
+	if (kstrtoint(buf, 10, &val))
+		return -EINVAL;
+
+	smb5_iio_set_prop(chg, PSY_IIO_SMART_CHARGING_INTERRUPTION, val);
+
+	return count;
+}
+
+static ssize_t smart_charging_interruption_show(struct class *class,
+				struct class_attribute *attr, char *buf)
+{
+	struct smb_charger *chg = container_of(class, struct smb_charger, bcext_class);
+	int val = 0;
+
+	smb5_iio_get_prop(chg, PSY_IIO_SMART_CHARGING_INTERRUPTION, &val);
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", val);
+}
+static CLASS_ATTR_RW(smart_charging_interruption);
+
+static struct attribute *somc_bcext_class_attrs[] = {
+	&class_attr_smart_charging_interruption.attr,
+	NULL,
+};
+ATTRIBUTE_GROUPS(somc_bcext_class);
+
 #endif
 static int smb5_probe(struct platform_device *pdev)
 {
@@ -4094,6 +4126,16 @@ static int smb5_probe(struct platform_device *pdev)
 		goto cleanup;
 	}
 
+#if defined(CONFIG_SOMC_CHARGER_EXTENSION)
+	chg->bcext_class.name = "battchg_ext";
+	chg->bcext_class.class_groups = somc_bcext_class_groups;
+	rc = class_register(&chg->bcext_class);
+	if (rc < 0) {
+		pr_err("Couldn't create bcext_class rc=%d\n", rc);
+		goto class_register;
+	}
+#endif
+
 	rc = smb5_request_interrupts(chip);
 	if (rc < 0) {
 		pr_err("Couldn't request interrupts rc=%d\n", rc);
@@ -4136,6 +4178,8 @@ static int smb5_probe(struct platform_device *pdev)
 
 free_irq:
 	smb5_free_interrupts(chg);
+class_register:
+	class_unregister(&chg->bcext_class);
 cleanup:
 	smblib_deinit(chg);
 	platform_set_drvdata(pdev, NULL);
@@ -4149,6 +4193,7 @@ static int smb5_remove(struct platform_device *pdev)
 	struct smb_charger *chg = &chip->chg;
 #if defined(CONFIG_SOMC_CHARGER_EXTENSION)
 	smb5_somc_remove_sysfs_entries(chg->dev);
+	class_unregister(&chg->bcext_class);
 #endif
 	/* force enable APSD */
 	smblib_masked_write(chg, USBIN_OPTIONS_1_CFG_REG,
